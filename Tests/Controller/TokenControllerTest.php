4a731273b863308bb5093b6fbe92b87ddc208f74
klapaudius <klapaudius@msn.com>
2025-04-29 22:55:21 +0200

    more tests

---
+++ Tests/Controller/TokenControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Controller;
+
+use FOS\OAuthServerBundle\Controller\TokenController;
+use OAuth2\OAuth2;
+use OAuth2\OAuth2ServerException;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Small]
+class TokenControllerTest extends TestCase
+{
+    private $serverMock;
+    private $tokenController;
+
+    protected function setUp(): void
+    {
+        $this->serverMock = $this->createMock(OAuth2::class);
+        $this->tokenController = new TokenController($this->serverMock);
+    }
+
+    public function testTokenActionReturnsAccessTokenResponse(): void
+    {
+        $request = $this->createMock(Request::class);
+        $response = $this->createMock(Response::class);
+
+        $this->serverMock
+            ->expects($this->once())
+            ->method('grantAccessToken')
+            ->with($request)
+            ->willReturn($response);
+
+        $result = $this->tokenController->tokenAction($request);
+
+        $this->assertSame($response, $result);
+    }
+
+    public function testTokenActionHandlesOAuth2ServerException(): void
+    {
+        $request = $this->createMock(Request::class);
+        $exceptionResponse = $this->createMock(Response::class);
+        $exception = $this->createMock(OAuth2ServerException::class);
+
+        $exception
+            ->expects($this->once())
+            ->method('getHttpResponse')
+            ->willReturn($exceptionResponse);
+
+        $this->serverMock
+            ->expects($this->once())
+            ->method('grantAccessToken')
+            ->with($request)
+            ->willThrowException($exception);
+
+        $result = $this->tokenController->tokenAction($request);
+
+        $this->assertSame($exceptionResponse, $result);
+    }
+}
